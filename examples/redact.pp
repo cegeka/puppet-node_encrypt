1d692358d3b913cb5ab67ab89ee98c2447e0bb46
Paul Herbosch <paul.herbosch@cegeka.be>
2019-08-01 10:24:13 +0200

    comment out examepl code to pass linter

--- examples/redact.pp
+++ examples/redact.pp
@@ -1,10 +1,12 @@
-class foo ($bar) {
-  notice("The value of bar is ${bar}")
-  redact('bar')
-}
-
-class { 'foo':
-  bar => 'buzzbuzz',
-}
-
-notice("The value of foo::bar is ${foo::bar}")
+# Example class
+#
+# class foo ($bar) {
+#   notice("The value of bar is ${bar}")
+#   redact('bar')
+# }
+# 
+# class { 'foo':
+#   bar => 'buzzbuzz',
+# }
+# 
+# notice("The value of foo::bar is ${foo::bar}")
